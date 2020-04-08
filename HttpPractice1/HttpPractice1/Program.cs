60dd18baef715d97a9a28f2b6af8e51bd9037652
Illia Abrosimov <lastsymbol0@gmail.com>
2020-04-08 20:29:49 +0300

    added colors

--- HttpPractice1/HttpPractice1/Program.cs
+++ HttpPractice1/HttpPractice1/Program.cs
@@ -5,8 +5,63 @@ using System.Text.RegularExpressions;
 using System.Net.Http;
 using System.Threading.Tasks;
 
+namespace ConsoleUtils
+{
+    public static class ConsoleUtils
+    {
+        public enum MessageType
+        {
+            COMMON_TEXT,
+            SUCCESS,
+            FAIL,
+            HINT,
+            WEAK
+        }
+
+        private static void EnableColors(MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.COMMON_TEXT:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case MessageType.SUCCESS:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case MessageType.FAIL:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case MessageType.HINT:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case MessageType.WEAK:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+        public static void WriteLine(string msg, MessageType type)
+        {
+            EnableColors(type);
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void WriteText(string msg, MessageType type)
+        {
+            EnableColors(type);
+            Console.Write(msg);
+            Console.ResetColor();
+        }
+    }
+}
+
 namespace HttpPractice1
 {
+    using ConsoleUtils;
+
     class MyClient
     {
         public void Init()
@@ -16,6 +71,7 @@ namespace HttpPractice1
             client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
             client.DefaultRequestHeaders.TryAddWithoutValidation("appId", "campus-task");
         }
+
         public async Task GetDataByIdRequest(string id) => await SendRequest(new HttpRequestMessage(HttpMethod.Get, id), false);
         public async Task GetAllDataRequest(bool isSorted) => await SendRequest(new HttpRequestMessage(HttpMethod.Get, isSorted ? "?sorted=True" : ""), false);
         public async Task DeleteDataByIdRequest(string id) => await SendRequest(new HttpRequestMessage(HttpMethod.Delete, id), true);
@@ -41,35 +97,50 @@ namespace HttpPractice1
         {
             var result = await client.SendAsync(message);
 
-            Console.WriteLine("Server responce:");
+            ConsoleUtils.WriteLine("Server responce:", ConsoleUtils.MessageType.WEAK);
             if (result.IsSuccessStatusCode)
             {
                 if (showOnlyReturnCode)
-                    Console.WriteLine($"Return code: {result.StatusCode}");
+                    ConsoleUtils.WriteLine($"Return code: {result.StatusCode}", ConsoleUtils.MessageType.SUCCESS);
                 else
-                    Console.WriteLine(await result.Content.ReadAsStringAsync());
+                    ConsoleUtils.WriteLine(await result.Content.ReadAsStringAsync(), ConsoleUtils.MessageType.SUCCESS);
             }
             else
             {
-                Console.WriteLine($"Return code: {result.StatusCode}\nMessage: {await result.Content.ReadAsStringAsync()}");
+                ConsoleUtils.WriteLine(
+                    $"Return code: {result.StatusCode}\nMessage: {await result.Content.ReadAsStringAsync()}",
+                    ConsoleUtils.MessageType.FAIL);
             }
-            Console.WriteLine();
         }
 
         private HttpClient client;
     }
 
-
     class Program
     {
         static async Task Main(string[] args)
         {
+            const string getUsage = "Get usage: get [--sorted (True/False)] [--id (dataId)]";
+            const string postUsage = "Post usage: post {\"dataId\": \"someId\", \"weight\": 42}";
+            const string putUsage = "Put usage: put {id} {\"dataId\": \"someId\", \"weight\": 42}";
+            const string deleteUsage = "Delete usage: delete {id}";
+
             MyClient client = new MyClient();
             client.Init();
 
+            ConsoleUtils.WriteLine(getUsage, ConsoleUtils.MessageType.HINT);
+            ConsoleUtils.WriteLine(postUsage, ConsoleUtils.MessageType.HINT);
+            ConsoleUtils.WriteLine(putUsage, ConsoleUtils.MessageType.HINT);
+            ConsoleUtils.WriteLine(deleteUsage, ConsoleUtils.MessageType.HINT);
+            ConsoleUtils.WriteLine("For exit enter 'q'", ConsoleUtils.MessageType.HINT);
+            Console.WriteLine();
+
             while (true)
             {
+                ConsoleUtils.WriteText("Command prompt >>> ", ConsoleUtils.MessageType.WEAK);
                 string command = Console.ReadLine();
+                Console.WriteLine();
+
                 if (command.Equals("q"))
                     break;
 
@@ -82,14 +153,14 @@ namespace HttpPractice1
                     else if (Regex.IsMatch(command, "^get --id .+ *$", RegexOptions.IgnoreCase))
                         await client.GetDataByIdRequest(command.Substring(9).Trim());
                     else
-                        Console.WriteLine("Get usage: get [--sorted (True/False)] [--id (dataId)]");
+                        ConsoleUtils.WriteLine(getUsage, ConsoleUtils.MessageType.HINT);
                 }
                 else if (Regex.IsMatch(command, "^post", RegexOptions.IgnoreCase))
                 {
                     if (Regex.IsMatch(command, "^post {.+} *$", RegexOptions.IgnoreCase))
                         await client.PostRequest(command.Substring(5).TrimEnd());
                     else
-                        Console.WriteLine("Post usage: post {\"dataId\": \"someId\", \"weight\": 42}");
+                        ConsoleUtils.WriteLine(postUsage, ConsoleUtils.MessageType.HINT);
                 }
                 else if (Regex.IsMatch(command, "^put", RegexOptions.IgnoreCase))
                 {
@@ -99,7 +170,7 @@ namespace HttpPractice1
                         await client.PutRequest(commandArgs[1], command.Substring(commandArgs[0].Length + commandArgs[1].Length + 2));
                     }
                     else
-                        Console.WriteLine("Put usage: put {id} {\"dataId\": \"someId\", \"weight\": 42}");
+                        ConsoleUtils.WriteLine(putUsage, ConsoleUtils.MessageType.HINT);
                 }
                 else if (Regex.IsMatch(command, "^delete", RegexOptions.IgnoreCase))
                 {
@@ -108,12 +179,15 @@ namespace HttpPractice1
                         await client.DeleteDataByIdRequest(command.Substring(7).Trim());
                     }
                     else
-                        Console.WriteLine("Delete usage: delete {id}");
+                        ConsoleUtils.WriteLine(deleteUsage, ConsoleUtils.MessageType.HINT);
                 }
                 else
                 {
-                    Console.WriteLine("Program: unknown command");
+                    ConsoleUtils.WriteLine("Program: unknown command", ConsoleUtils.MessageType.FAIL);
                 }
+
+                Console.WriteLine();
+                Console.WriteLine();
             }
         }
     }
